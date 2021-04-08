d9454e0a91de12b4ed2f9291bb49805df6ac7299
Aditya Gautam <60175624+aditya-tezsure@users.noreply.github.com>
2021-04-08 19:23:24 +0530

    fixing stakepool UI for version 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,33 @@
 import React from 'react';
 
-import logo from '../../assets/images/logo.png';
+import logo from '../../assets/images/Group 35964@2x.png';
 
 const Header = (props) => {
-    return(
+    return (
         <header className="site-header">
             <div className="site-logo">
                 <a href="/" className="site-header-link">
-                    <img src={logo} alt="logo"/>
+                    <img src={logo} alt="logo" className="logo-image" />
                 </a>
             </div>
 
             <ul className="site-menu-wrapper">
                 <li className="site-menu-item">
-                    <a href="#" className="site-menu-item-link">Documentation</a>
+                    <a href="#" className="site-menu-item-link">
+                        Documentation
+                    </a>
                 </li>
                 <li className="site-menu-item">
-                    <a href="#" className="site-menu-item-link">Github</a>
+                    <a href="#" className="site-menu-item-link">
+                        Github
+                    </a>
                 </li>
                 <li className="site-menu-item">
                     <button className="contact-btn">Get in Touch</button>
                 </li>
             </ul>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
