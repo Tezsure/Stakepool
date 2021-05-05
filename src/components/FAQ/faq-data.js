d2d4619ff564741b1de1996fab9458d6b5e5da3e
Bernd Oostrum <berndoostrum@gmail.com>
2021-05-05 22:11:34 +0200

    Update based on user feedback

--- src/components/FAQ/faq-data.js
+++ src/components/FAQ/faq-data.js
@@ -2,12 +2,13 @@ module.exports = [
     {
         question: 'What is Stakepool',
         answer:
-            'Stakepool is a platform where you can earn Tez by predicting the Tezos price at the end of a staking period. Your staking rewards are what\'s at stake! ',
+            'With Stakepool you can earn bonus staking rewards by predicting the future Tez price range in USD value. You win if you correctly predict the Tez price range at the completion of the 2 cycles that come after the cycle in which you placed your bet. All baking rewards accrued in the smart contract are distributed among the winners as weighted averages of the total staked amount. If your prediction is wrong, your staked Tez is returned but you miss out on your staking rewards for the respective cycles.',
     },
     {
         question: 'How does it work',
         answer:
-            '',
+            'Let\'s give an example: We are currently in cycle 1. Alice and Bob think that they know which price Tezos will be after the coming staking period (2 cycles). They both place a bet on Stakepool. Bob is bullish and Alice is bearish.\n Alice and Bob wait for 2 cycles. Plenty of time to attract more friends to stakepool and increase potential juicy rewards for future predictions.\nCycle 4 starts and the price is up! Bob has won! He can withdraw his staked XTZ and of course his juicy stakepool bonus. Alice is wrong this time, she can withdraw her staked Tez to give it another try. Unfortunately no staking rewards for Alice this time. 
+',
     },
     {
         question: 'How do I get Started',
@@ -21,18 +22,12 @@ module.exports = [
             'In other prediction markets you lose your bet if your prediction turns out to be wrong, here in Stakepool you get your bet back. If you win then you get rewards just like any other prediction market.',
     },
     {
-        question: 'For how long is my stake delegated to a baker?',
+        question: 'For how long is my stake delegated to a baker',
         answer:
             'Stakes are delegated two cycles after the cycle you placed your bet (so starting at cycle 150 means your stake is delegated until cycle 152). We are calling this the staking period',
     
     },
     {
-        question:
-            'When and how is a winner of a particular staking period chosen',
-        answer:
-            'Considering the above scenario, at the beginning of cycle at the beggining of 153rd cycle once we update the winning price of last cycle is updated to our contract, bettors can then withdraw there rewards but invoking a particular entry point.',
-    },
-      {
         question: 'Is my money safe',
         answer:
             'Absolutely! Only you can withdraw your stake. We suggest you go through our smart contract to better understand our authorization checks.',
